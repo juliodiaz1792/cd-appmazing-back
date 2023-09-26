2a7cdaffe21c7d49eb317861b1858aa634d8b8b7
Julio <juliodiaz1792@gmail.com>
2023-09-26 11:37:25 +0200

    First step CONTACT table

---
+++ src/main/java/com/campusdual/appmazing/model/Contact.java
@@ -0,0 +1,60 @@
+package com.campusdual.appmazing.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CONTACT")
+public class Contact {
+    //Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String first_surname;
+    @Column
+    private String second_surname;
+    @Column
+    private int phone;
+    @Column
+    private String mail;
+
+    //Getter and setter
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getFirst_surname() {
+        return first_surname;
+    }
+    public void setFirst_surname(String first_surname) {
+        this.first_surname = first_surname;
+    }
+    public String getSecond_surname() {
+        return second_surname;
+    }
+    public void setSecond_surname(String second_surname) {
+        this.second_surname = second_surname;
+    }
+    public int getPhone() {
+        return phone;
+    }
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
