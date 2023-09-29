0761fd8b0446a15f150326b27eba4bdea1245cf0
Julio <juliodiaz1792@gmail.com>
2023-09-29 09:18:01 +0200

    Creation of the MODEL

---
+++ src/main/java/com/campusdual/appmazing/model/Category.java
@@ -0,0 +1,30 @@
+package com.campusdual.appmazing.model;
+
+import jdk.jfr.Name;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CATEGORY")
+public class Category {
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
